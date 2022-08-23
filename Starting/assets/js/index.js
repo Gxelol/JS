6cba98509087d86649bc8f18f524b17774208ec8
Gabriel <gabrielqgoncalves26@gmail.com>
2022-08-23 12:44:35 -0300

    Starting Java's basics

---
+++ Starting/assets/js/index.js
@@ -0,0 +1,77 @@
+console.log("Hello 'World'"); //String
+console.log('Hello "World"');
+console.log(`'Hello' "World"`)
+
+console.log(26, 35.4); // Number
+
+console.log('Hello, my name is "Gabriel". I am learning JavaScript at', 01,'pm');
+
+// Hello World. This is just a comment, in Java it is used with two bars. Just one line comment.
+
+/* This it's a comment with more than one line.
+and I can break to other lines.*/
+
+console.log("Line", 11);
+console.log("Line", 12);
+console.log("Line", 13);    // This line it's the 13th.
+console.log("Line", 14);
+//console.log("Line", 15);
+
+let nameClient;
+
+console.log('Are you', nameClient);
+console.log(nameClient, "have eighteen years old");
+console.log("My name is also", nameClient);
+console.log("Can I write your name, ", nameClient, "?");
+console.log(2022);
+
+let lant; //undefined
+let lent = 32; //defined
+
+const stringNumber = '4';
+const firstNumber = 3;
+const secondNumber = 8;
+const product = firstNumber * secondNumber;
+const doubleProduct = product * 2;
+
+console.log(product);
+console.log(doubleProduct);
+
+console.log(typeof(stringNumber));
+
+const namePerson = "Person";
+const age = 29;
+const weightKg = 70;
+const heightCm = 1.78;
+let bodyMassIndex; 
+let birthYear; 
+
+bodyMassIndex = weightKg / (heightCm * heightCm);
+birthYear= 2022 - age;
+
+console.log(`${namePerson} has ${age} years old, she was born in ${birthYear}, her weight is ${weightKg} kg, her height is ${heightCm} meters and her BMI is ${bodyMassIndex}.`);
+
+const numb1 = 2;
+const numb2 = 10;
+
+// console.log(numb1 operator numb2);
+
+/*
+
+- - subtraction
+* - multiplication
+/ - division
++ - addition
+** - potentiation
+% - rest 
+
+*/
+
+let counter = 1
+++counter;
+// --counter;
+console.log(counter);
+
+//parseInt - Convert to an integer
+//parseFloat - Convert to an float number
+
