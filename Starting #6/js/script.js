0230889715e085474e00cc833c51c4b702aa8f36
Gabriel <gabrielqgoncalves26@gmail.com>
2022-08-25 13:10:59 -0300

    Starting #6

---
+++ Starting #6/js/script.js
@@ -0,0 +1,26 @@
+// function greeting(nameClient) {
+//     return `Good Morning ${nameClient}!`;
+// }
+
+// greeting(prompt('Type your name: '));
+
+// function sumOfNumbers(x, y) {
+//     const sum = x + y;
+//     return sum;
+// };
+
+// const num1 = Number(prompt('Type a number:'));
+// const num2 = Number(prompt('One more:'));
+
+// const result = sumOfNumbers(num1, num2);
+
+// const clientSum = document.getElementById('sum')
+
+// clientSum.innerHTML = `The sum is = ${result}`;
+
+// const root = raiz => raiz ** 0.5
+
+// console.log(root(9));
+// console.log(root(25));
+// console.log(root(36));
+
