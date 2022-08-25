b3271b2ce59a627a30aa6a74c0873674559d832d
Gabriel <gabrielqgoncalves26@gmail.com>
2022-08-24 22:01:03 -0300

    Starting #5

---
+++ Starting #5/js/script.js
@@ -0,0 +1,15 @@
+const students = ['Gabriel', 'Raphael', 'Samuel'];
+// console.log(students[0]);
+// students[2] = 'Daniel'
+
+console.log(typeof students);
+console.log(students instanceof Array);
+
+// students.unshift('Ezreal')
+// students.push('Ezreal');
+
+// const removedStudent = students.pop(); //students.shift();
+// students[students.length] = 'Israel';
+
+// console.log(removedStudent);
+console.log(students);
