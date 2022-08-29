51c75996779540f0ba0455c05e61115b3075ae40
Gabriel <gabrielqgoncalves26@gmail.com>
2022-08-29 18:24:58 -0300

    if, else and else if

---
+++ Programming Logic #3/js/script.js
@@ -0,0 +1,39 @@
+// if
+// else
+// else if
+
+// const time = 16;
+// if (time >=0 && time <= 11) {
+//     console.log('Good Morning');
+// } else if (time > 11 && time <=17) {
+//     console.log('Good afternoon');
+// } else if (time > 17 && time < 23) {
+//     console.log('Good night');
+// } else {
+//     console.log('Hello');
+// }
+
+// const haveTime = true;
+// if (haveTime) {
+//     console.log('I will play');
+// } else {
+//     console.log('I will not play ');
+// }
+
+// const num1 = 20;
+// if (num1 >= 0) {
+//     console.log('Yes, the number is bigger or equal to zero');
+// } else {
+//     console.log('No, the number is smaller than 0');
+// }
+
+const num2 = 9;
+if (num2 >= 0 && num2 < 5) {
+    console.log('The number is between 0 and 5');
+} else if (num2 >= 5 && num2 < 8) {
+    console.log('The number is between 5 and 8');
+} else if (num2 >= 8 && num2 < 13) {
+    console.log('The number is between 8 and 13');
+} else {
+    console.log(`The number is not between 0 and 13.`);
+}
