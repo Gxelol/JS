797f5405671f52133e642d98c4a443b665ed9270
Gabriel <gabrielqgoncalves26@gmail.com>
2022-08-29 12:57:26 -0300

    Comparison operators

---
+++ Programming Logic #1/js/script.js
@@ -0,0 +1,32 @@
+/* 
+>
+>=
+<
+<=
+== (not recommended)
+===
+!= (not recommended)
+!==
+*/
+
+console.log(10 >= 5);
+console.log(10 < 10);
+console.log(10 === 10);
+console.log(20 <= 5);
+console.log(10 !== 5);
+console.log(10 > 5);
+
+const ex = (5 < 6);
+console.log(ex);
+
+const ex2 = (14 <= 8);
+console.log(ex2);
+
+const num1 = 10;
+const num2 = 20;
+const ex3 = (num1 < num2);
+console.log(ex3);
+
+const num3 = 31;
+const num4 = '32';
+console.log(num3 === num4);
