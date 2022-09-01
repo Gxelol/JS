57ec93edfa926176ea98afc15e9b1d3423fce6ca
Gabriel <gabrielqgoncalves26@gmail.com>
2022-09-01 17:59:12 -0300

    learning the classic for

---
+++ Programming Logic #14/js.script.js
@@ -0,0 +1,22 @@
+// console.log('line 0');
+// console.log('line 1');
+// console.log('line 2');
+// console.log('line 3');
+// console.log('line 4');
+// console.log('line 5');
+
+// for (let i = 0; i <= 100; i += 10) {
+//     console.log(i);          // for it's used for repetitions
+// }
+
+// for (let  i = 0; i <=10; i++) {
+//     const evenOdd = i % 2 === 0 ? `Your number is even` : `Your number is odd`;
+//     console.log(i, evenOdd);
+// }
+
+const food = ['rice', 'beans', 'steak', 'spaguetti'];
+
+for (let i = 0; i < food.length; i++) {
+    console.log(`Index ${i}`, food[i]);
+}
+
