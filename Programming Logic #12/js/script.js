c87bcdf34151143ebd5942acbbcccc6c9b854ecf
Gabriel <gabrielqgoncalves26@gmail.com>
2022-09-01 11:05:04 -0300

    destructuring assignment

---
+++ Programming Logic #12/js/script.js
@@ -0,0 +1,13 @@
+// const numbers = [10, 22, 43, 56, 85, 36, 67, 78, 98];
+// let [firstNumber, secondNumber, thirdNumber, fourthNumber, ...rest]  = numbers;  // ... = rest / spread
+// console.log(firstNumber, secondNumber, thirdNumber, fourthNumber);
+// console.log(rest);
+
+
+// const lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+// const [,,[,eight]] = lists;
+// console.log(eight);      // COMPLEX 
+
+const lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+const [list1, list2, list3] = lists;
+console.log(list2[1]);      // EASIER TO UNDERSTAND
