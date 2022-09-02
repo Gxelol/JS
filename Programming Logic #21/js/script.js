c3547f886879b6f53b5b40fd3b86f787a0175e70
Gabriel <gabrielqgoncalves26@gmail.com>
2022-09-02 18:40:47 -0300

    Is the image a landscape? Function problem

--- Programming Logic #21/js/script.js
+++ Programming Logic #21/js/script.js
@@ -1,5 +1,6 @@
 function eLandscape (width, height) {
-    height < width ? console.log(`${true} --- height: ${height}, width: ${width}`) : console.log(`${false} --- height: ${height}, width: ${width}`);
+    height < width ? console.log(`Is the image a landscape? ${true} --- height: ${height}, width: ${width}`) : 
+    console.log(`Is the image a landscape?${false} --- height: ${height}, width: ${width}`);
 }
 
 eLandscape(Math.floor(Math.random() * 100), Math.floor(Math.random() * 100))
