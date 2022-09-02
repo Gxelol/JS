0c1310e520b7188633e36b78a968ef05abc215a1
Gabriel <gabrielqgoncalves26@gmail.com>
2022-09-02 14:29:18 -0300

    break and continue

---
+++ Programming Logic #19/js/script.js
@@ -0,0 +1,30 @@
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// for (let i of numbers) {
+    
+//     if (i === 3 || i === 6) {
+//         continue;
+//     }
+
+//     console.log(i);
+// }
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// for (let i of numbers) {
+    
+//     if (i === 3 || i === 6) {
+//         continue;
+//     }
+
+//     console.log(i);
+// }
+
+
+for (let i = 0; i <= 500; i++) {
+    if (i === 46) {
+        break;
+    }
+    console.log(i)
+}
+
