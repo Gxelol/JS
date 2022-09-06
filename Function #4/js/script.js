eac73994d64e2ea5a4a5ea37b2c4e2dc48eed012
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-09-06 19:23:32 -0300

    Lexical scoping

---
+++ Function #4/js/script.js
@@ -0,0 +1,13 @@
+const nameC = 'Ytalo';
+
+function sayName() {
+    // const nameC = 'hebrain';
+    console.log(nameC);
+}
+
+function useSayName() {
+    const nameC = 'hebrain';
+    sayName();
+}
+
+useSayName();
