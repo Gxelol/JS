45b36d50f049decb1ff44c447b579590de0a93d2
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-09-09 21:41:20 -0300

    reviewing the basic of array

---
+++ Arrays #1/js/script.js
@@ -0,0 +1,43 @@
+// const names = ['Edgar', 'Maria', 'Joanne']
+// names[2] = 'Paulo';
+// // delete names[2];
+// console.log(names);
+
+//Strings, Objects, Functions, Numbers, etc..;
+
+// const constructorArray = new Array('WOLRD', 'pe', 'killua');
+// constructorArray[2] = 'Hello';
+// console.log(constructorArray);
+
+// const names = ['Edgar', 'Maria', 'Joanne']
+// const n2 = [...names];
+// // n2.pop();
+// names[1] = 'Henry';
+// console.log(names);
+// console.log(n2);
+
+// console.log(names.length);
+
+// const removed = names.pop(); 
+// console.log(names, removed);
+
+// const remStart = names.shift();
+// console.log(remStart, names, removed);
+
+// const names = ['Edgar', 'Maria', 'Joanne']
+// names.push('Wallace'); //add in the end
+// names.unshift('Larry'); //add in the beginning
+
+// const sliced = names.slice(1, 4)
+// const sliced = names.slice(1, -1)
+
+// console.log(sliced);
+
+// const nameC = `Larry the lobster`;
+// const names = nameC.split(' ');
+
+// console.log(names);
+
+const names = ['Larry', 'the', 'lobster']
+const nameC = names.join(' ');
+console.log(nameC);
