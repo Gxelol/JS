948553692b5c1a098cc8b1b03a9d73f5743aee6d
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-09-09 22:08:36 -0300

    arrays concatenation

---
+++ Arrays #3/js.script.js
@@ -0,0 +1,11 @@
+// const array1 = [0, 1, 2];
+// const array2 = [3, 4, 5];
+
+// const array3 = array1.concat(array2, [7, 8, 9], 'Gabriel');
+// console.log(array3);
+
+const array1 = [0, 1, 2];
+const array2 = [3, 4, 5];
+
+const array3 = [...array1, ...array2];
+console.log(array3);
