97564e30eaea45dbbecbc85f3054200604b41390
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-09-09 21:55:00 -0300

    splice method

---
+++ Arrays #2/js/script.js
@@ -0,0 +1,19 @@
+// const names = ['Gabriel', 'Larry', 'Maria', 'João', 'Tadeu'];
+
+// // names.splice(index, delete, add1, add2, add3, ...)
+// const removed = names.splice(3, 2, 'Jerry'); //Number.MAX_VALUE
+// console.log(names, removed);
+
+const names = ['Gabriel', 'Larry', 'Maria', 'João', 'Tadeu'];
+
+//pop
+// names.splice(-1, 1);
+
+//shift
+// names.splice(0, 1);
+
+//push
+// names.splice(names.length, 0, 'Element');
+
+//unshift
+// names.splice(0, 0, 'Element');
