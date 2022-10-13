de9b08beeff6cc1f335128f78cabfd9bb464bcff
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2022-10-13 13:39:00 -0300

    validating CPFs and IPs

--- Regular Expressions/base.js
+++ Regular Expressions/base.js
@@ -18,6 +18,24 @@ const files = [
   'Blend.png',
 ]
 
+const cpf = `
+Os CPFs são:
+  254.224.877-45 215.978.456-12 047.258.369-96 
+  963.987.321-00
+  963.987.32a.00 (NÃO VÁLIDO)
+  963.987.32-00 (NÃO VÁLIDO)
+`;
+
+const ips = `
+Os Ips são:
+   0.0.0.0
+   192.168.0.25
+        10.10.5.12
+        10.01.10.20 (ESTE IP NÃO É VÁLIDO)
+        10.021.08.20 (ESTE IP NÃO É VÁLIDO)
+   255.255.255.255
+`;
+
 //            <tag>something</tag>   <tag> text </tag>
 const html = ' <p>Hello world</p>   <p>Hello again</p> <div>I am the div</div>';
 
@@ -27,5 +45,7 @@ module.exports = {
   text,
   files,
   html,
-  alphabet
+  alphabet,
+  cpf,
+  ips
 }
